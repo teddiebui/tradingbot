c6d1121869ea7b53787470313045d81f8cdb759b
teddiebui <74448657+teddiebui@users.noreply.github.com>
2021-03-04 22:52:27 +0800

    Add files via upload

--- binanceApi.py
+++ binanceApi.py
@@ -16,8 +16,6 @@ from binance.enums import *
 from binance.client import Client
 from binance.exceptions import BinanceAPIException, BinanceOrderException
 
-import binanceApi.crawler.priceCrawler as pc
-import binanceApi.crawler.candleCrawler as cc
 import binanceApi.tradingBot.tradingBot as tb
 import binanceApi.tradingBot.alertBot as ab
 
@@ -39,40 +37,11 @@ class MainApplication:
 	def run(self):
 		print("main run")
 
-		for symbol in (self.symbols):
+		bot = tb.TradingBot(self.client, self.symbols[0])
+		bot.start()
 
-			bot = tb.TradingBot(self.client, symbol)
-			self.BOTS.append(bot)
-			# bot.start()
-			break
+		self.BOTS.append(bot)
 
-		bot.order_maker.create_order_buy_market()
-		# self.alert_bot.alert()
-
-		#TESTING PURPOSE
-		# orders = self.client.get_all_orders(symbol='BNBUSDT', limit=10)
-
-		# order_status = self.client.get_order(
-		#     symbol='BNBUSDT',
-		#     orderId='1629496042')
-
-		# fees = self.client.get_trade_fee()
-
-		# #TESING PURPOSE
-		# order = client.create_order(
-  #   symbol='BNBBTC',
-  #   side=SIDE_BUY,
-  #   type=ORDER_TYPE_LIMIT,
-  #   timeInForce=TIME_IN_FORCE_GTC,
-  #   quantity=1,
-  #   price=100)
-
-
-		# # pprint.pprint(order_status)
-		# # pprint.pprint(orders[:])
-		# pprint.pprint(fees)
-
-		
 
 if __name__ == "__main__":
 
