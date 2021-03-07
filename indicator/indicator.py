94237460b86b8225979121e6c0b064fcbb9a6585
teddiebui <74448657+teddiebui@users.noreply.github.com>
2021-03-07 21:45:58 +0800

    Add files via upload

--- indicator/indicator.py
+++ indicator/indicator.py
@@ -6,7 +6,7 @@ class Indicator():
 
 	def __init__(self):
 		#rsi indicator variables
-		self.OVERSOLD_THRESHOLD = 30
+		self.OVERSOLD_THRESHOLD = 16
 		self.OVERBOUGHT_THRESHOLD = 70
 		self.rsi = []
 
@@ -25,15 +25,38 @@ class Indicator():
 
 		print("...indicator created")
 
+	def update(self, candles):
+
+		closes = [i['close'] for i in candles]
+
+		self.refresh_rsi(closes)
+		self.refresh_macd(closes)
+		self.refresh_ema_200(closes)
+		self.refresh_dmi(candles)
+
+		# print()
+		# print("rsi: ", self.rsi[-4:])
+		# print()
+		# print("macd: ", self.macd[-4:])	
+		# print("macd signal: ", self.macd_signal[-4:])	
+		# print("macd histogram: ", self.macd_hist[-4:])	
+		# print()
+		# print("ema 200: ", self.ema_200[-4:]) 
+		# print()
+		# print("adx: ", self.adx[-4:])
+		# print("+di: ", self.plus_di[-4:])
+		# print("-di: ", self.minus_di[-4:])
+		# print()
+		# print("---------------------")
+
+
 	def refresh_rsi(self, close_list):
 
 		# CALCULATE by callback function _refresh_indicator
 		self.rsi = list(talib.RSI(numpy.array(close_list)))
 		# just for prettifying
 		self.rsi = list(map(lambda x: round(x,2), self.rsi))	
-		#DEBUG 
-		print()
-		print("rsi: ", self.rsi[-4:])
+		
 
 	def refresh_macd(self, close_list):
 
@@ -45,15 +68,9 @@ class Indicator():
 			signalperiod=9)
 
 		# just for prettifying
-		self.macd = list(map(lambda x: round(x,2), self.macd))
-		self.macd_signal = list(map(lambda x: round(x,2), self.macd_signal))
-		self.macd_hist = list(map(lambda x: round(x,2), self.macd_hist))
-
-		#DEBUG
-		print()
-		print("macd: ", self.macd[-4:])	
-		print("macd signal: ", self.macd_signal[-4:])	
-		print("macd histogram: ", self.macd_hist[-4:])	
+		self.macd = list(map(lambda x: round(x,4), self.macd))
+		self.macd_signal = list(map(lambda x: round(x,4), self.macd_signal))
+		self.macd_hist = list(map(lambda x: round(x,4), self.macd_hist))
 
 	def refresh_ema_200(self, close_list):
 
@@ -61,10 +78,6 @@ class Indicator():
 		self.ema_200 = talib.EMA(numpy.array(close_list),200)
 		# just for prettifying
 		self.ema_200 = list(map(lambda x: round(x,2), self.ema_200))
-
-		#DEBUG
-		print()
-		print("ema 200: ", self.ema_200[-4:])
 
 	def refresh_dmi(self, candles):
 
@@ -89,40 +102,6 @@ class Indicator():
 		self.adx = list(map(lambda x: round(x,2), self.adx))
 		self.plus_di = list(map(lambda x: round(x,2), self.plus_di))
 		self.minus_di = list(map(lambda x: round(x,2), self.minus_di))
-
-		#DEBUG 
-		print()
-		print("adx: ", self.adx[-4:])
-		print("+di: ", self.plus_di[-4:])
-		print("-di: ", self.minus_di[-4:])
-
-		print()
-		print("---------------------")
-
-
-
-	def validate_rsi(self):
-
-		#TODO: viet logic validate rsi
-		if self.rsi[-1] < self.OVERSOLD_THRESHOLD:
-			return True
-		return False
-
-	def validate_macd(self):
-
-		#TODO: viet logic validate macd
-
-
-		if self.macd[-2] < 0 and self.macd_signal[-2] < 0: #filter
-			return False
-
-		if self.macd[-1] < 0 and self.macd_signal[-1] < 0: #filter
-			return False
-
-		# check if macd crossover signal
-
-
-		return True
 
 
 if __name__ == "__main__":
