340311bcdf28181bc5bc97f2ed1ceb9c95f39925
sapphiral <barry.bbui@gmail.com>
2021-03-19 19:31:56 +0800

    minor update for tradingbot

--- tradingBot/tradingBot.py
+++ tradingBot/tradingBot.py
@@ -18,7 +18,7 @@ from .alertBot import AlertBot
 
 class TradingBot(threading.Thread):
 
-    def __init__(self, client, symbol, algorithm, order_maker, indicator):
+    def __init__(self, client, symbol, algorithm, indicator, order_maker = None, order_manager = None):
         threading.Thread.__init__(self)
         self.is_running = False
 
@@ -28,29 +28,26 @@ class TradingBot(threading.Thread):
         self.algorithm = algorithm
         self.order_maker = order_maker
         self.indicator = indicator
+        self.order_manager = order_manager
 
         #initiating objects
         self.alert_bot = AlertBot()
         self.candle_crawler = cc.CandleCrawler(client, symbol)
 
     def crawl_klines(self):
-        # CALCULATE EVERY MINTUE
-        self.candle_crawler.start_crawling(callback1=self._check_if_can_buy, callback2=self.order_maker.check_current_position)
+        # CALCULATE EVERY INTERVAL TIME
+        self.candle_crawler.start_crawling(callback1=self._check_if_can_buy, callback2=self.order_maker)
         
     def _check_if_can_buy(self):
 
-        #TODO: finish trading algorithm for bot
-        if not self.order_maker.is_in_position:
-            # find a signal to buy if currently not in position
-            try:
-                signal = self.algorithm.run(self.candle_crawler.candles_15m, self.indicator)
-                print(self.indicator.rsi[-7:])
-                if signal == True:
-                    self.alert_bot.run()
-                    #MAKE ORDER
-                    pass
-            except Exception as e:
-                print("..._chcek if can buy: ", e)
+        # RUN ALGPRITHM CHECK
+        signal = self.algorithm.run(self.candle_crawler.candles_15m, self.indicator)
+        
+        if signal == True:
+            self.alert_bot.run()
+            #MAKE ORDER
+            self.order_manager.buy()
+            pass
 
     def run(self):
         self.is_running = True
@@ -61,7 +58,6 @@ class TradingBot(threading.Thread):
 
 
     def stop(self):
-        self.report()
         self.is_running = False
 
         self.candle_crawler.stop()
