d240dba08f1c447c43f86da329ed350034661e9e
teddiebui <74448657+teddiebui@users.noreply.github.com>
2021-03-20 14:24:55 +0800

    update alert bot

--- tradingBot/alertBot.py
+++ tradingBot/alertBot.py
@@ -5,23 +5,23 @@ import threading
 
 class AlertBot:
 
-	def __init__(self):
-		pass
-	def alert(self):
-		try:
-			path = os.path.dirname(__file__) + "\\alert.m4a"
-			for i in range(3):
-				playsound.playsound(path)
-				time.time()
-		except Exception as e:
-			print(e)
-	
-	def run(self):
-		threading.Thread(target=self.alert, args=()).start()
+    def __init__(self):
+        pass
+    def alert(self):
+        try:
+            #TEMPORARILY DISABLE THIS FUNCTION
+            return
+            # path = os.path.dirname(__file__) + "\\alert.m4a"
+            # playsound.playsound(path)
+        except Exception as e:
+            print(e)
+    
+    def run(self):
+        threading.Thread(target=self.alert, args=()).start()
 
 if __name__ == "__main__":
 
-	# back testing
-	
-	a = AlertBot()
-	a.run()
+    # back testing
+    
+    a = AlertBot()
+    a.run()
